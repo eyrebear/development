b081110e0cd9ac451180abc76fe904e904d9e23b
ford eyre <fordeyre@gmail.com>
2022-05-18 21:07:20 -0600

    dom-debug complete, white default to be able to accept red on first change event listener selection

---
+++ dom-debug/index.js
@@ -0,0 +1,40 @@
+const colors = ["", "red", "blue", "green"]
+
+document.getElementById("add").addEventListener("click", function(e){
+    const subItem = createSubItem(e)
+    document.getElementById("list").appendChild(subItem)
+})
+
+function createDropDown(){
+    const dropDown = document.createElement("select")
+
+    for (let i = 0; i < colors.length; i++){
+        const option = document.createElement("option") 
+        option.innerHTML = colors[i]
+        option.value = colors[i]
+        dropDown.append(option)
+    }
+
+    dropDown.addEventListener("change", function(e){
+        e.target.parentElement.style.backgroundColor = e.target.value
+    })
+    return dropDown
+}
+
+function createSubItem(e){
+    const subItem = document.createElement("div")
+    var subItemValue = document.getElementById("input")
+    subItem.textContent = subItemValue.value
+
+    const dropDown = createDropDown()
+    subItem.appendChild(dropDown)
+    dropDown.parentElement.style.backgroundColor = colors[0]
+    subItem.setAttribute("class", "subItem")
+    console.log(subItem)
+    console.log(subItemValue)
+    console.log(subItemValue)
+    return subItem
+    
+}
+
+
