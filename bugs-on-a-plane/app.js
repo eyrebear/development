2edd7c191d2a6ca8727fa77cf41594a5c4c941eb
ford eyre <fordeyre@gmail.com>
2022-05-29 16:50:29 -0600

    two button fixes, script src fix

---
+++ bugs-on-a-plane/app.js
@@ -0,0 +1,38 @@
+var form = document.getElementById("airline-form"); //var form = ducument["airline-form"]
+var submit = document.getElementById("submit");
+//var query = document.querySelector;
+
+//This isn't called anywhere, is it?
+function formAlert() {
+    //e.preventDefault();
+    let firstName = form.elements["firstName"].value;
+        form.elements["firstName"].value = "";
+    let lastName = form.elements["lastName"].value;
+        form.elements["lastName"].value = "";
+    let age = form.elements["age"].value;
+        form.elements["age"].value = "";
+    const gender = form.elements["gender"].value;
+    let location = form.elements["travelLocation"].value;
+    let diet = [];
+    if (form.vegan.checked) {
+        diet.push(document.getElementById("vegan").value);
+    } if (form.gluten.checked) {
+        diet.push(document.getElementById("gluten").value);
+    } if (form.paleo.checked) {
+        diet.push(document.getElementById("paleo").value);
+    }
+
+    alert(`First Name: ${firstName} Last Name: ${lastName} \nAge: ${age} \nGender: ${gender} \nTravel Location: ${location} \nDiet:  ${diet} \nAwesome, now if you die, it won't be an accident..`);
+ } 
+
+
+
+// document.getElementById("submit").
+ form.addEventListener("submit", function(e){
+     e.preventDefault();
+           formAlert();
+ 
+ })
+//form.addEventListener("submit", formAlert);
+    
+
