1b6f5a0c2840b5a1658b885e3a4bda8ca9b820d9
ford eyre <fordeyre@gmail.com>
2023-03-18 23:00:31 -0600

    name-badge basic title renders and file started

---
+++ name-badge/src/App.jsx
@@ -0,0 +1,15 @@
+import React , {useState} from "react"
+import Navbar from "./components/Navbar"
+
+
+export default function App() {
+
+    return <>
+    <nav id="navbar">
+        <Navbar />
+    </nav>
+    <h2 className="title">Name Badge</h2>
+    
+    </>
+
+}
