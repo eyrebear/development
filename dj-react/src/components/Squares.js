d8ab861f555487da2267fa3298888825621201f0
ford eyre <fordeyre@gmail.com>
2023-03-18 20:55:10 -0600

    added squares

---
+++ dj-react/src/components/Squares.js
@@ -0,0 +1,7 @@
+import React, {useState} from "react"
+
+export default function Squares(props) {
+    return(
+        <div className="box" style={{backgroundColor: props.color}} onClick={props.handleClick}></div>
+    )
+}
