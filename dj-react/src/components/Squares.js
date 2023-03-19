dacdd63b2c6159eac8607e51a16bb9169cb123c7
ford eyre <fordeyre@gmail.com>
2023-03-18 21:04:17 -0600

    testing git issues

--- dj-react/src/components/Squares.js
+++ dj-react/src/components/Squares.js
@@ -1,5 +1,6 @@
 import React, {useState} from "react"
 
+
 export default function Squares(props) {
     return(
         <div className="box" style={{backgroundColor: props.color}} onClick={props.handleClick}></div>
